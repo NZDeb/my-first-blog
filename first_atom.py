5cd6cd51f346f5a2fd13beccbf773ae201161044
Deb Burgoyne <deb@rata.co.nz>
2019-03-23 12:30:19 +1300

    My Django Girls app, first commit

---
+++ first_atom.py
@@ -0,0 +1,14 @@
+print('Hello Django girls')
+participant = {
+    'name': 'Deb',
+    'country': 'NZ',
+    'hobbies': [
+        'reading',
+        'cross-stitch',
+        'gardening'
+    ],
+    'best food': 'chocolate'
+}
+numbers = [2,4,6,8,10]
+print(participant)
+print(numbers)
